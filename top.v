ab4d0f0c4975a7399e474e416dbc49d7390b358c
Soumyadeep Singh <151494995+soumyaC204@users.noreply.github.com>
2024-12-14 19:31:57 +0530

    Update top.v

--- top.v
+++ top.v
@@ -1,14 +1,20 @@
 module harmonica_top(
-    input wire clk,
-    input wire play,
+    input wire clk,          
+    input wire play,             
     input wire [2:0] swar_select,
-    output wire pwm_out
+    output wire pwm_out          
 );
+    wire clk_8k;                  
     wire [7:0] sample_data;
     wire [12:0] sample_address;
 
+    clock_divider clk_div (
+        .clk_in(clk),
+        .clk_out(clk_8k)
+    );
+
     address_counter addr_cnt (
-        .clk(clk),
+        .clk(clk_8k),
         .play(play),
         .address(sample_address)
     );
