f183799a4c6c7ac6e0184a0a345704f5dc87c321
Soumyadeep Singh <151494995+soumyaC204@users.noreply.github.com>
2024-12-18 12:46:39 +0530

    Update pwm.v

--- pwm.v
+++ pwm.v
@@ -1,12 +1,15 @@
-module pwm_generator(
+module pwm_generator (
     input wire clk,
-    input wire [7:0] sample,
+    input wire rst,
     output reg pwm_out
 );
-    reg [7:0] pwm_counter;
 
-    always @(posedge clk) begin
-        pwm_counter <= pwm_counter + 1;
-        pwm_out <= (pwm_counter < sample) ? 1 : 0;
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            pwm_out <= 0;
+        end else begin
+            pwm_out <= ~pwm_out;
+        end
     end
+
 endmodule
