a2383eac6140c67948126db458ba4489a5c08970
Soumyadeep Singh <151494995+soumyaC204@users.noreply.github.com>
2024-12-18 12:45:36 +0530

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,15 +1,24 @@
-module clock_divider(
-    input wire clk_in,       
-    output reg clk_out     
+module clock_divider (
+    input wire clk,
+    input wire rst,
+    input wire [31:0] div_value,
+    output reg clk_out
 );
-    reg [13:0] counter = 0; 
 
-    always @(posedge clk_in) begin
-        if (counter == 12499) begin
+    reg [31:0] counter = 0;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
             counter <= 0;
-            clk_out <= ~clk_out;
+            clk_out <= 0;
         end else begin
-            counter <= counter + 1;
+            if (counter >= div_value) begin
+                counter <= 0;
+                clk_out <= ~clk_out;
+            end else begin
+                counter <= counter + 1;
+            end
         end
     end
+
 endmodule
